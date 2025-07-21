3fc56defbe2dd7e6df615ca3452c213f5f61c618
Xander313 <jhonn.roldan8626@utc.edu.ec>
2025-07-21 15:58:14 -0500

    Dep,y

--- HydroSys/settings.py
+++ HydroSys/settings.py
@@ -36,9 +36,6 @@ INSTALLED_APPS = [
     'Aplicaciones.Notificaciones',
     'Aplicaciones.LogsUsuario',
     'Aplicaciones.PWA',
-    'admin_black'
-
-
 ]
 
 MIDDLEWARE = [
@@ -138,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'rjhoonalex@gmail.com'
+EMAIL_HOST_PASSWORD = 'qjtn nabh nhau hihh'
 
 
 
